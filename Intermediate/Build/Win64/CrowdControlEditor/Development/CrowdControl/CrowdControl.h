fd3b0d485afbba7d48e2e95ec177b61bb9083a91
ElliotCouvignou <ecouvignou@gmail.com>
2024-09-25 13:56:47 -0700

    Remove old stuff

--- Intermediate/Build/Win64/CrowdControlEditor/Development/CrowdControl/CrowdControl.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_CROWDCONTROL_H__
-#define __AUTO_CROWDCONTROL_H__
-//Last Write: 11/23/2023 5:41:43 PM +00:00
-#include "C:\Users\Adam\Documents\Unreal Projects\CrowdControl\Source\CrowdControl\Public\CrowdControl.h"
-#endif//__AUTO_CROWDCONTROL_H__
--- Source/CrowdControl/Private/CCEvents.cpp
+++
@@ -1,8 +0,0 @@
-#include "CrowdControl.h"
-#include "CCEvents.h"
-
-UCCEvents::UCCEvents(const class FPostConstructInitializeProperties& PCIP)
-    : Super(PCIP)
-{
-    // Constructor code here
-}
